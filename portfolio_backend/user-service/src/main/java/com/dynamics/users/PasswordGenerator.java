67abf56e14e8be37db3bbf66163f6bfe26913709
andilosing <andreas.losing@gmx.de>
2024-11-07 09:16:15 +0100

    change example raw password

--- portfolio_backend/user-service/src/main/java/com/dynamics/users/PasswordGenerator.java
+++ portfolio_backend/user-service/src/main/java/com/dynamics/users/PasswordGenerator.java
@@ -5,7 +5,7 @@ import com.dynamics.users.entity.User;
 public class PasswordGenerator {
     public static void main(String[] args) {
         User user = new User();
-        String rawPassword = "password123?";
+        String rawPassword = "xxx";
         user.setPassword(rawPassword);
         System.out.println("Generated BCrypt Hash: " + user.getPassword());
     }
